0c47abfcb06d66e8f974b1809fc35f99d6f5fc2c
Can Mingir <canmingir@gmail.com>
2023-04-16 10:29:25 -0400

    Send message to 2 consumers with different groupIds

--- src/test/mock.spec.js
+++ src/test/mock.spec.js
@@ -1,13 +1,13 @@
 const Mock = require("../../");
 const { equal } = require("assert");
 
-describe("Kafka Adapter", () => {
-  it("should register", async () => {
-    const kafka = new Mock.Kafka({
-      clientId: "test-client-1",
-      brokers: ["test"],
-    });
+const kafka = new Mock.Kafka({
+  clientId: "test-client-1",
+  brokers: ["test"],
+});
 
+describe("Mock Kafka", () => {
+  it("sends and receives message", async () => {
     const producer = kafka.producer();
     const consumer = kafka.consumer({ groupId: "test-group-1" });
 
@@ -34,5 +34,49 @@ describe("Kafka Adapter", () => {
     });
 
     equal(assert, true);
+  });
+  it("sends message to 2 consumers with different groupIds", async () => {
+    const producer = kafka.producer();
+    const consumer1 = kafka.consumer({ groupId: "test-group-1" });
+    const consumer2 = kafka.consumer({ groupId: "test-group-2" });
+
+    await producer.connect();
+    await consumer1.connect();
+    await consumer2.connect();
+
+    let assert1 = false;
+    let assert2 = false;
+
+    await consumer1.subscribe({ topic: "test-topic-1" });
+    await consumer2.subscribe({ topic: "test-topic-1" });
+
+    await consumer1.run({
+      eachMessage: async ({ topic, message }) => {
+        if (
+          topic === "test-topic-1" &&
+          message.value.toString() === "TEST_MESSAGE"
+        ) {
+          assert1 = true;
+        }
+      },
+    });
+    await consumer2.run({
+      eachMessage: async ({ topic, message }) => {
+        if (
+          topic === "test-topic-1" &&
+          message.value.toString() === "TEST_MESSAGE"
+        ) {
+          assert2 = true;
+        }
+      },
+    });
+
+    await producer.send({
+      topic: "test-topic-1",
+      messages: [{ value: "TEST_MESSAGE" }],
+    });
+
+    equal(assert1, true);
+    equal(assert2, true);
   });
 });
